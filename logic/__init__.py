d0418d49d94b5fd06d45e246e4cc38682f983569
liuzhengwen <15011265322@163.com>
2018-05-25 19:33:44 +0800

    全局db和redis_store处理

--- logic/__init__.py
+++ logic/__init__.py
@@ -3,8 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
 from flask_session import Session
-from config import ProductionConfig, DevelopmentConfig, UnittestConfig
 from config import configs
+
+
+# 创建SQLAlchemy的对象
+db = SQLAlchemy()
+# 创建redis的对象
+redis_store = StrictRedis()
 
 
 def create_app(config_name):
@@ -13,10 +18,12 @@ def create_app(config_name):
     # 配置文件的加载
     app.config.from_object(configs[config_name])
     # 创建连接到SQLAlchemy的对象
-    db = SQLAlchemy(app)
+    # db = SQLAlchemy(app)
+    db.init_app(app)
 
     # 创建连接到Redis的数据库对象
-    redis_store = StrictRedis(host=configs[config_name].REDIS_HOST, port=configs[config_name].REDIS_PORT)
+    # redis_store = StrictRedis(host=configs[config_name].REDIS_HOST, port=configs[config_name].REDIS_PORT)
+    redis_store.__init__(host=configs[config_name].REDIS_HOST, port=configs[config_name].REDIS_PORT)
 
     # 开启 CSRF保护：当不适用flaskForm表单类， 但是需要用post请求方法是需自己开启CSRF保护
     CSRFProtect(app)
--- manage.py
+++ manage.py
@@ -1,15 +1,16 @@
-from logic import create_app
+from logic import create_app, db, redis_store
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+
 
 # 创建app
 app = create_app('dev')
 # 创建脚本管理对象
 manger = Manager(app)
 # 让迁移和app和db建立联系
-# Migrate(app, db)
+Migrate(app, db)
 # 将迁移的脚本命令添加到manager
-# manger.add_command("mysal", MigrateCommand)
+manger.add_command("mysal", MigrateCommand)
 
 
 @app.route('/')
