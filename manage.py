69c4e0a68346a2347463cccf298d8e47ac7b22f3
liuzhengwen <15011265322@163.com>
2018-05-25 11:17:59 +0800

    add config

--- manage.py
+++ manage.py
@@ -1,6 +1,17 @@
 from flask import Flask
 
+
+class Config(object):
+    """配置文件的加载"""
+
+    # 开启调试模式
+    DEBUG = True
+
+
 app = Flask(__name__)
+
+# 配置文件的加载
+app.config.from_object(Config)
 
 
 @app.route('/')
