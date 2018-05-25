48b0aa7086afcfbf2871665c4f450239adb2f319
liuzhengwen <15011265322@163.com>
2018-05-25 15:30:32 +0800

    CSRFProtect继承

--- manage.py
+++ manage.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf import CSRFProtect
+import os,base64
+
 
 
 
 class Config(object):
     """配置文件的加载"""
 
+    # 配置秘钥 项目中的CSRF 和session需要用到
+    SECRET_KEY = base64.b64encode(os.urandom(148))
     # 开启调试模式
     DEBUG = True
 
@@ -32,6 +37,9 @@ db = SQLAlchemy(app)
 
 # 创建连接到Redis的数据库对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+#开启 CSRF保护：当不适用flaskForm表单类， 但是需要用post请求方法是需自己开启CSRF保护
+CSRFProtect(app)
 
 
 @app.route('/')
