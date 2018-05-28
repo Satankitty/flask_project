5884d9f8042f65674fd87c0161c69c4cb6bdd08b
liuzhengwen <229387042@qq.com>
2018-05-28 19:27:18 +0800

    首页title左侧图标处理

--- logic/modules/index/views.py
+++ logic/modules/index/views.py
@@ -1,9 +1,14 @@
 # 视图函数
 # 导入蓝图对象
 from . import index_blue
-from flask import render_template
+from flask import render_template, current_app
 
 
 @index_blue.route('/')
 def index():
-    return render_template('/news/index.html')
+    return render_template('news/index.html')
+
+
+# @index_blue.route('/favicon.ico', methods=['GET'] )
+# def favicon():
+#     return current_app.send_static_file('news/favicon.ico')
