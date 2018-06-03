f91c6e823b4823ba72ee4b7c0c77914adf93dfeb
liuzhengwen <229387042@qq.com>
2018-06-03 14:56:10 +0800

    修改抽取模板新闻详情页点击排行渲染

--- logic/modules/news/views.py
+++ logic/modules/news/views.py
@@ -1,8 +1,29 @@
+from logic import constants
+from logic.models import User, News
 from . import news_blue
-from flask import render_template
+from flask import render_template, session, current_app
 
 
 @news_blue.route('/detail/<int:news_id>')
 def news_detail(news_id):
+    """新闻详情"""
+    # 1. 获取登录用户信息
+    user_id = session.get('user_id', None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    # 2. 点击排行:查询新闻数据,根据clicks点击属性实现倒序
+    news_clicks = []
+    try:
+        news_clicks = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
 
-    return render_template('news/detail.html')
+    context = {
+        'user':user,
+        'news_clicks':news_clicks
+    }
+    return render_template('news/detail.html',context=context)
